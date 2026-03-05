fba8c56fe475d79a680c81ce591494166cecad0f
janidu22 <janidudhakshi222@gmail.com>
2026-03-05 22:41:10 +0530

    changed the logo

--- src/components/layout/PublicLayout.tsx
+++ src/components/layout/PublicLayout.tsx
@@ -50,7 +50,10 @@ export function PublicLayout() {
         <div className="max-w-7xl mx-auto flex items-center justify-between">
           <Link to="/" className="flex items-center gap-3">
             <div className="w-10 h-10 bg-salon-gold rounded-full flex items-center justify-center text-white font-serif text-xl font-bold shadow-inner">
-              <img src="/public/logo.png" alt="logo" />
+              <img
+                src="https://res.cloudinary.com/dg3rk2yot/image/upload/v1772730569/logo_eyv3fr.png"
+                alt="logo"
+              />
             </div>
             <div className="flex flex-col">
               <span className="text-xl font-serif font-bold tracking-tight text-white">
